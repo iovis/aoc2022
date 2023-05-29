b8d4a81e7af3369821f1dab687133fa2d3e0bd88
David Marchante <davidmarchan@gmail.com>
2023-05-29 13:30:13 -0400

    day7: format tree like in AoC example to compare

--- crates/day7/src/file_system_entry.rs
+++ crates/day7/src/file_system_entry.rs
@@ -1,7 +1,16 @@
-#[derive(Debug, PartialEq, Eq)]
+#[derive(PartialEq, Eq)]
 pub enum FileSystemEntry {
     Directory(Directory),
     File(File),
+}
+
+impl std::fmt::Debug for FileSystemEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Directory(dir) => write!(f, "{} (dir)", dir.name),
+            Self::File(file) => write!(f, "{} (file, size={})", file.name, file.size),
+        }
+    }
 }
 
 // TODO: Use references
